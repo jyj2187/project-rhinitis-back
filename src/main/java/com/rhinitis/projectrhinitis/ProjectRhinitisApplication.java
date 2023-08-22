ca0b06c57a8aef1b0f83c5c0fafd99231fa0042b
Llems 정윤조 <43122133+jyj2187@users.noreply.github.com>
2023-08-22 22:13:00 +0900

    project init

---
+++ src/main/java/com/rhinitis/projectrhinitis/ProjectRhinitisApplication.java
@@ -0,0 +1,13 @@
+package com.rhinitis.projectrhinitis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRhinitisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRhinitisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rhinitis/projectrhinitis/ProjectRhinitisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhinitis.projectrhinitis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRhinitisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
